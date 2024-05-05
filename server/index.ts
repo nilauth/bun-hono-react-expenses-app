6a02a4b67e99db054fb271a3f90ded0548d0fc8f
nilauth <nilauth.dev@gmail.com>
2024-05-05 21:13:55 +0100

    port

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import app from "./app";
-Bun.serve({
+const server = Bun.serve({
+  port: process.env.PORT || 3000,
   fetch: app.fetch,
 });
 
-console.log("Server running!");
+console.log("Server running!", server.port);
